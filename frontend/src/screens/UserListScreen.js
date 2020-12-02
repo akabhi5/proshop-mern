137816eb938e5eeceac89f98e3079f5298117799
Abhishek Kumar <abhishekk580@gmail.com>
2020-12-02 23:53:36 +0530

    initial commit

---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserListScreen = () => {
+    const dispatch = 
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserListScreen
